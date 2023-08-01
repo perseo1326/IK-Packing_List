6d2f04b2df24c803096e7ea73f86eee728599e56
John W <perseo1326@gmail.com>
2023-08-01 22:08:00 +0200

    Load and read data file using promises

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 
 'use strict';
 
-
 const footerVersion = document.getElementById("version-footer");
+const shipmentData = document.getElementById("shipment-data");
+
+const loadingFrame = document.getElementById("loading-frame");
+const loadFileLabel = document.getElementById("shipment-data-label");
+
+const dataTable = document.getElementById("data");
+
 
 
 const VERSION = "1.0";
 
+let contentData = [];
 
 
 
+
+    // *********************************************************
+    // Event Listeners
+
+    shipmentData.addEventListener('change', openFile );
 
 
     // *********************************************************
@@ -21,16 +33,65 @@ const VERSION = "1.0";
     // Function to initialize the original values
     function initializePage() {
         console.log("Inicializando valores originales...");
-        document.title = "Generador Packing List - GALEX";
+        document.getElementById("title").innerText = document.title = "Generador Packing List - GALEX";
         
         footerVersion.innerText = "Versión " + VERSION + footerVersion.innerText;
+        contentData = [];
+
 
     }
 
+    // *********************************************************
+    // Function to read a selected file
+    function openFile(evento) {
+        
+        dataTable.innerHTML = "";
+        let html = "";
 
-    
+        let file = evento.target.files[0];
+        loadingFrame.classList.remove("no-visible");
 
+        let fileStatus = new ExcelFileOpen(file);
 
+        loadFileLabel.innerText = fileStatus.file.name;
+
+        const promiseData = loadExcelFile(fileStatus);
+
+        promiseData.then( (response) => {
+            contentData = response;
+
+            contentData.forEach( ( row ) => {
+
+                html += "<tr>";
+                console.log("FILA: ", row);
+                
+                    for (const cell in row) {
+                        if (Object.hasOwnProperty.call( row, cell)) {
+                            const element = row[cell];
+                            console.log("Celda : ", element);
+                            html += "<td>";
+                            html += element;
+                            html += "</td>";
+                        }
+                    }
+                    html += "</tr>";
+                });
+                dataTable.innerHTML = html;
+            })
+            .catch( (error) => {
+                console.log("ERROR:openFile: ", error);
+                alert(error.message);
+                initializePage();
+            })
+            .finally( () => {
+                loadingFrame.classList.add("no-visible");
+                // process and clean info from the file
+                // let arrayExcel = readReportsExcel(excelFile.file, contentFile);
+                console.log("Carga \"" + fileStatus.file.name + "\" Finalizada!", contentData); 
+            });
+    }
+
+    // *********************************************************
 
 
     
--- excelReportsFunctions.js
+++ excelReportsFunctions.js
@@ -15,32 +15,37 @@ class ExcelFileOpen {
 }
 
 
+const EXCEL_MIME_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+const WORKING_SHEET = "Sheet1";
+
+
+
     // *********************************************************
     // Verify the valid structure of data readed from the file based on the headers of info
     function validateContentExcel(dataRows) {
         
-        if(dataRows === undefined || dataRows.length <= 0 ) {
-            return false;
-        }
+        // if(dataRows === undefined || dataRows.length <= 0 ) {
+        //     return false;
+        // }
 
-        if(dataRows[0][ORDER_TYPE] === undefined || 
-            dataRows[0][PICK_AREA] === undefined ||
-            dataRows[0][CUT_OFF_DATE] === undefined ||
-            dataRows[dataRows.length - 1][CUT_OFF_TIME] === undefined ||
-            dataRows[dataRows.length - 1][ISELL] === undefined) {
-            return false;
-        }
+        // if(dataRows[0][ORDER_TYPE] === undefined || 
+        //     dataRows[0][PICK_AREA] === undefined ||
+        //     dataRows[0][CUT_OFF_DATE] === undefined ||
+        //     dataRows[dataRows.length - 1][CUT_OFF_TIME] === undefined ||
+        //     dataRows[dataRows.length - 1][ISELL] === undefined) {
+        //     return false;
+        // }
     
-        return true;
+        // return true;
     }
 
 
     // *********************************************************
     // Check and remove all elements with "Order Type" different that "PUP"
     function filterOrdersByDate(dataArray, textDate) {
-        return dataArray.filter( (row) => { 
-            return row[CUT_OFF_DATE].trim() === textDate;
-        } );
+        // return dataArray.filter( (row) => { 
+        //     return row[CUT_OFF_DATE].trim() === textDate;
+        // } );
     }
     
 
@@ -49,11 +54,11 @@ class ExcelFileOpen {
 
         let excelDataArray = fileDataArray;
 
-        // check the file type
-        if(file === undefined || (!file.name.toLowerCase().endsWith(".xlsx") && file.type !== EXCEL_MIME_TYPE) ) {
-            console.log("ERROR:readReportsExcel: El archivo \"" + file.name + "\" NO es válido.");
-            throw new Error("El archivo \"" + file.name + "\" NO es válido.");
-        }
+        // // check the file type
+        // if(file === undefined || (!file.name.toLowerCase().endsWith(".xlsx") && file.type !== EXCEL_MIME_TYPE) ) {
+        //     console.log("ERROR:readReportsExcel: El archivo \"" + file.name + "\" NO es válido.");
+        //     throw new Error("El archivo \"" + file.name + "\" NO es válido.");
+        // }
 
         // Validate the format of the file and data structure
         if(!validateContentExcel(excelDataArray)) {
@@ -65,23 +70,56 @@ class ExcelFileOpen {
 
 
     // *********************************************************
-    function mappingArrayDataExcel(dataArrayExcel) {
+    function validateMymeType(file){
 
-        let orderDetailsMap = new Map();
-        dataArrayExcel.forEach(rowData => {
-            
-            if(!orderDetailsMap.has(rowData[ISELL])) {
-                let newOrder = new Order(rowData);
-                orderDetailsMap.set(rowData[ISELL], newOrder);
-            }
-            
-            let producto = new Product(rowData);
-
-            let order = orderDetailsMap.get(rowData[ISELL]);
-
-            order.addProduct(producto, rowData[PICK_AREA]);
-
-            orderDetailsMap.set(rowData[ISELL], order);
-        });
-        return orderDetailsMap;
+        // check the file type
+        if(file === undefined || (!file.name.toLowerCase().endsWith(".xlsx") && file.type !== EXCEL_MIME_TYPE) ) {
+            console.log("ERROR:readReportsExcel: El archivo \"" + file.name + "\" NO es válido.");
+            throw new Error("El archivo \"" + file.name + "\" NO es válido.");
+        }
+        console.log("Validado Myme type");
     }
+
+    
+    // *********************************************************
+    function loadExcelFile(excelFile){
+    
+        return new Promise( (resolve, reject ) => {
+
+            console.log("INFO:loadExcelFile: Iniciando lectura de archivo.");
+            
+            let fileReader = new FileReader();
+            // Constants for minification
+            const read = "read";
+            const utils = "utils";
+            const sheet_to_row_object_array = "sheet_to_row_object_array";
+            const Sheets = "Sheets";
+                
+            validateMymeType(excelFile.file);
+            
+            fileReader.readAsArrayBuffer(excelFile.file);
+            fileReader.onload = function(){
+                
+                try {
+                    let buffer = this.result;
+                    let workbook =  XLSX[read](buffer);
+                    let contentExcelFile = XLSX[utils][sheet_to_row_object_array](workbook[Sheets][WORKING_SHEET]);
+                    
+                    resolve(contentExcelFile);
+                    
+                } catch (error) {
+                    console.log("ERROR:loadExcelFile: ", error);
+                    alert(error.message);
+                    initializePage();
+                    reject(error);
+                }
+            };
+        });
+    }
+
+
+
+
+
+
+
