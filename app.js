29cd56d98c191910aa0ae659dae6778af812cc05
John W <perseo1326@gmail.com>
2023-08-07 22:33:21 +0200

    reading, validate and process file + add visual for show shipments and controls

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
 
 'use strict';
 
+class TruckInfo {
+
+    constructor (shipmentId, estimatedArrival ){
+        this.shipmentId = shipmentId.trim();
+        this.arrivalText(estimatedArrival.trim());
+    }
+
+    arrivalText ( text ){
+        const data = text.split(" ");
+        const splitDate = data[0].split('/');
+        const splitTime = data[1].split(':');
+        this.estimatedArrivalDate = new Date( splitDate[2], (splitDate[1] - 1), splitDate[0], splitTime[0], splitTime[1] );
+        this.isPlants = (splitTime[0] == 10 ? true : false );
+        this.code = 0;
+        // console.log("Creando Fechas: ", text, "NUEVA: ", this.estimatedArrivalDate.toLocaleString());
+    }
+}
+
 const footerVersion = document.getElementById("version-footer");
 const shipmentData = document.getElementById("shipment-data");
 
 const loadingFrame = document.getElementById("loading-frame");
 const loadFileLabel = document.getElementById("shipment-data-label");
 
-const dataTable = document.getElementById("data");
+const dataTable = document.getElementById("shipments");
+const shipmentsTotal = document.getElementById("shipments-total");
+const shipmentsClear = document.getElementById("shipments-clear");
+const shipmentsRemove = document.getElementById("shipments-remove");
 
 
 
 const VERSION = "1.0";
 
-let contentData = [];
+const SHOP_CODE_LONG = "Receiver ID ="; // Begins With 406-STO-1";
+const SHOP_CODE_ID = "406-STO-1";
+
+// Column name and "number of column"
+const SHIPMENT_COL = { colName : "Shipment ID", columnNumber : "" } ;
+const RECEIVER_COL = { colName : "Receiver", columnNumber : "" };
+const ESTIMATE_ARRIVAL_DATE = { colName : "Estimated Arrival Date", columnNumber : "" };
+
+
+// let contentData = [];
+let shipmentsData = [];
 
 
 
@@ -21,7 +52,16 @@ let contentData = [];
     // *********************************************************
     // Event Listeners
 
-    shipmentData.addEventListener('change', openFile );
+    shipmentData.addEventListener('change', openTrucksInfoFile );
+
+    shipmentsClear.addEventListener('click', () => {
+        const inputSelected = document.querySelectorAll('input[type=checkbox]:checked');
+
+        inputSelected.forEach( element => {
+            element.checked = false;
+        });
+        // console.log("INPUTS: ", document.querySelectorAll('input[type=checkbox]:checked'));
+    });
 
 
     // *********************************************************
@@ -34,19 +74,20 @@ let contentData = [];
     function initializePage() {
         console.log("Inicializando valores originales...");
         document.getElementById("title").innerText = document.title = "Generador Packing List - GALEX";
+        loadFileLabel.innerText = "Información de Shipments";
         
         footerVersion.innerText = "Versión " + VERSION + footerVersion.innerText;
-        contentData = [];
+        // contentData = [];
+        shipmentsData = [];
 
 
     }
 
     // *********************************************************
     // Function to read a selected file
-    function openFile(evento) {
+    function openTrucksInfoFile(evento) {
         
-        dataTable.innerHTML = "";
-        let html = "";
+        let arrayDataTrucks = [];
 
         let file = evento.target.files[0];
         loadingFrame.classList.remove("no-visible");
@@ -58,43 +99,122 @@ let contentData = [];
         const promiseData = loadExcelFile(fileStatus);
 
         promiseData.then( (response) => {
-            contentData = response;
+            
+            const contentData = validateFile( response );
+            console.log("Carga \"" + fileStatus.file.name + "\" Finalizada!", contentData); 
 
-            contentData.forEach( ( row ) => {
+            arrayDataTrucks = getInfoFromGrossData( contentData );
 
-                html += "<tr>";
-                console.log("FILA: ", row);
-                
-                    for (const cell in row) {
-                        if (Object.hasOwnProperty.call( row, cell)) {
-                            const element = row[cell];
-                            console.log("Celda : ", element);
-                            html += "<td>";
-                            html += element;
-                            html += "</td>";
-                        }
-                    }
-                    html += "</tr>";
-                });
-                dataTable.innerHTML = html;
-            })
-            .catch( (error) => {
-                console.log("ERROR:openFile: ", error);
-                alert(error.message);
-                initializePage();
-            })
-            .finally( () => {
-                loadingFrame.classList.add("no-visible");
-                // process and clean info from the file
-                // let arrayExcel = readReportsExcel(excelFile.file, contentFile);
-                console.log("Carga \"" + fileStatus.file.name + "\" Finalizada!", contentData); 
-            });
+            arrayDataTrucks = sortTrucksInfo(arrayDataTrucks);
+            // console.log("Array data Trucks: ", arrayDataTrucks);
+
+            document.getElementById("button-load-shipments").classList.add("no-visible");
+            document.getElementById("shipments-container").classList.remove("no-visible");
+            document.getElementById("shipments-commands").classList.remove("no-visible");
+            shipmentsTotal.innerText = arrayDataTrucks.length;
+
+            shipmentsData = arrayDataTrucks;
+
+            showShipmentsData(arrayDataTrucks);
+        })
+        .catch( (error) => {
+            console.log("ERROR:openFile: ", error);
+            alert(error.message);
+            initializePage();
+        })
+        .finally( () => {
+            loadingFrame.classList.add("no-visible");
+        });
     }
 
     // *********************************************************
+    function validateFile(grossExcelData) {
 
+        let shipmentId = false;
+        let receiver = false;
+        let estimatedArrivalDate = false;
+        let shopCode = false;
+        let isfirstAppearance = false;
 
+        for (const row of grossExcelData) {
+            // console.log("FILA: ", row);
+            
+            for (const cell in row) {
+                if (Object.hasOwnProperty.call( row, cell)) {
+                    const element = row[cell];
+
+                    // console.log("Celda : ", element, cell );
+                    if( element.includes( SHOP_CODE_LONG ) ) {
+                        shopCode = element.includes( SHOP_CODE_ID ) ? true : false ;
+                        // console.log("RReceiver ID code :", element);
+                    }
+
+                    if( element === SHIPMENT_COL.colName ){
+                        SHIPMENT_COL.columnNumber = cell;
+                        // console.log("Encontrado primer encabezado: ", element, SHIPMENT_COL);
+                        shipmentId = true;
+                    }
+
+                    if( element === RECEIVER_COL.colName ) {
+                        RECEIVER_COL.columnNumber = cell;
+                        // console.log("Encontrado SEGUNDO encabezado: ", element, RECEIVER_COL );
+                        receiver = true;
+                    }
+
+                    if ( element === ESTIMATE_ARRIVAL_DATE.colName ) {
+                        
+                        if(isfirstAppearance){
+                            ESTIMATE_ARRIVAL_DATE.columnNumber = cell;
+                            // console.log("Encontrado TERCER encabezado: ", element, ESTIMATE_ARRIVAL_DATE);
+                            estimatedArrivalDate = true;
+                        }
+                        isfirstAppearance = true;
+                    }
+
+                    if( shopCode && shipmentId && receiver && estimatedArrivalDate ) {
+                        console.log("Columnas encontradas: ", SHIPMENT_COL, RECEIVER_COL, ESTIMATE_ARRIVAL_DATE );
+                        return grossExcelData;
+                    }
+                }
+            }
+        }
+        console.log("ERROR:validateFile: Estructura del archivo NO válida.")
+        throw new Error("Estructura del archivo NO válida.");
+    }
+
+    // *********************************************************
+    function getInfoFromGrossData( grossDataArray ){
+
+        const arrayData = [];
+
+        for (const row of grossDataArray) {
+            // console.log("FILA INFO: ", RECEIVER_COL, row[RECEIVER_COL.columnNumber], row[RECEIVER_COL.colName] );
+            if( row[RECEIVER_COL.columnNumber] === SHOP_CODE_ID ){
+
+                // console.log("VALORES ORIGINALES: ", row);
+                // console.log("truckInfoRow = ", row[SHIPMENT_COL.columnNumber ], ESTIMATE_ARRIVAL_DATE, row[ESTIMATE_ARRIVAL_DATE.columnNumber]);
+
+                let truckInfoRow = new TruckInfo( row[SHIPMENT_COL.columnNumber ], row[ESTIMATE_ARRIVAL_DATE.columnNumber]);
+                // console.log("TruckInfo objeto: ", truckInfoRow );
+                arrayData.push(truckInfoRow);
+            }
+        }
+        console.log("Cantidad de filas obtenidas: ", arrayData.length );
+        return arrayData;
+    }
     
+    // *********************************************************
+    function sortTrucksInfo( arrayData ){
+
+        arrayData.sort( ( a, b ) => {
+            // console.log("Fechas: ", a.estimatedArrivalDate, b.estimatedArrivalDate, a.estimatedArrivalDate.getTime() < b.estimatedArrivalDate.getTime() ? -1 : 1  );
+            return ( a.estimatedArrivalDate.getTime() < b.estimatedArrivalDate.getTime() ? -1 : 1 );
+        });
+
+        return arrayData;
+    }
+
+
     // *********************************************************
     // *********************************************************
 
